40bda772e00e95397fd6eeeb1f1b4cbb63a99544
Anmol <anmolkamboj1656@gmail.com>
2026-09-22 20:12:01 +0530

    ab

--- Apipro/src/components/Navbar.jsx
+++ Apipro/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
 
   return ( 
    <header className='m-5'> 
-    <nav className='flex justify-between items-center py-3 px-20 bg-gray-800 text-white rounded-full'> 
+    <nav className='flex justify-between items-center py-3 px-15  bg-gray-800 text-white rounded-full w-full'> 
  
         {/* left section */} 
         <h1 className='text-2xl font-bold'>AN</h1> 
@@ -18,14 +18,14 @@ export default function Navbar() {
  
         {/* centre section */} 
  
-        <ul className='flex  hiddenmd:flex gap-15 '> 
+        <ul className=' hidden md:flex gap-15 '> 
             <li>All users</li> 
             <li>All projects</li> 
             <li>All Stocks</li> 
         </ul> 
  
         {/* right section */} 
-        <ul className='flex gap-10'> 
+        <ul className=' hidden md:flex gap-8'> 
             <li><FaLinkedin className='text-2xl' /></li> 
             <li><FaInstagram className='text-2xl' /></li> 
             <li><FaGithub className='text-2xl' /></li> 
@@ -33,7 +33,7 @@ export default function Navbar() {
  
         <div onClick={()=> setOpen(!Open)} className='text-2xl  md:hidden '> 
              { 
-                Open ? <FaBars/>:<IoMdClose /> 
+                Open ? <IoMdClose /> : <FaBars/> 
  
              } 
         </div> 
