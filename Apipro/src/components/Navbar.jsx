2f0c0bac26da5be11fe5c01bab4d08ee01f7000c
Anmol <anmolkamboj1656@gmail.com>
2026-09-22 18:46:55 +0530

    ab

--- Apipro/src/components/Navbar.jsx
+++ Apipro/src/components/Navbar.jsx
@@ -1,28 +1,43 @@
-import React from 'react'
-import { FaLinkedin ,FaInstagram ,FaGithub } from "react-icons/fa";
+import {useState} from 'react' 
+import { FaLinkedin ,FaInstagram ,FaGithub ,FaBars } from "react-icons/fa"; 
+import { IoMdClose } from "react-icons/io"; 
+ 
+ 
+ 
+export default function Navbar() { 
 
+  const[Open, setOpen] = useState(false); 
 
-export default function Navbar() {
-  return (
-   <header calassName='m-5'>
-    <nav className='flex justify-between items-center p-5 bg-gray-800 text-white'>
-        {/* left section */}
-        <h1>AN</h1>
-        {/* centre section */}
-
-        <ul>
-            <li>All users</li>
-            <li>All projects</li>
-            <li>All Stocks</li>
-        </ul>
-
-        {/* right section */}
-        <ul>
-            <li><FaLinkedin /></li>
-            <li><FaInstagram /></li>
-            <li><FaGithub /></li>
-        </ul>
-    </nav>
-   </header>
-  )
+  return ( 
+   <header className='m-5'> 
+    <nav className='flex justify-between items-center py-3 px-20 bg-gray-800 text-white rounded-full'> 
+ 
+        {/* left section */} 
+        <h1 className='text-2xl font-bold'>AN</h1> 
+ 
+ 
+        {/* centre section */} 
+ 
+        <ul className='flex  hiddenmd:flex gap-15 '> 
+            <li>All users</li> 
+            <li>All projects</li> 
+            <li>All Stocks</li> 
+        </ul> 
+ 
+        {/* right section */} 
+        <ul className='flex gap-10'> 
+            <li><FaLinkedin className='text-2xl' /></li> 
+            <li><FaInstagram className='text-2xl' /></li> 
+            <li><FaGithub className='text-2xl' /></li> 
+        </ul> 
+ 
+        <div onClick={()=> setOpen(!Open)} className='text-2xl  md:hidden '> 
+             { 
+                Open ? <FaBars/>:<IoMdClose /> 
+ 
+             } 
+        </div> 
+    </nav> 
+   </header> 
+  ) 
 }
